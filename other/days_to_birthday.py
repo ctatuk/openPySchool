46ed13384998ad4582a47050ab4f66d554409798
Volaner <myemail2004@inbox.ru>
2021-02-05 23:12:24 +0300

    fix days_to_birthday.py

--- other/days_to_birthday.py
+++ other/days_to_birthday.py
@@ -2,9 +2,10 @@ from datetime import date
 
 def days_to_birtday(month, day):
     today = date.today()
-    current_year = date.today().strftime("%Y")
-    birthday_in_this_year = date(int(current_year), month, day)
-    birthday_in_next_year = date(int(current_year) + 1, month, day)
+    # current_year = date.today().strftime("%Y")
+    current_year = date.today().year
+    birthday_in_this_year = date(current_year, month, day)
+    birthday_in_next_year = date(current_year + 1, month, day)
 
     if birthday_in_this_year == today:
         return 'Happy birthday!'
@@ -13,4 +14,6 @@ def days_to_birtday(month, day):
     else:
         return abs((birthday_in_next_year - today).days)
 
+print(days_to_birtday(1, 5))
 print(days_to_birtday(2, 5))
+print(days_to_birtday(3, 15))
